9b990ac15e5316b417693d3d358d3015ff4bd965
User <user@STX-MI-00002.local>
2021-05-15 12:33:02 -0300

    adding api weather

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,7 +1,38 @@
-
+import React, { useState, useEffect} from 'react'
+import axios from 'axios';
 import styles from './styles.module.scss';
 
 export function Header() {
+
+  const [location, setLocation] = useState (false);
+  const [weather, setWeather] = useState(false);
+
+
+  let getWeather = async (lat, long) => {
+    let res = await axios.get("http://api.openweathermap.org/data/2.5/weather", {
+      params: {
+        lat: lat,
+        lon: long,
+        appid: process.env.REACT_APP_OPEN_WEATHER_KEY,
+        lang: 'pt',
+      }
+    });
+  }
+
+
+  useEffect(() =>{
+    navigator.geolocation.getCurrentPosition((position)=>{
+      console.log(position.coords.latitude, position.coords.longitude);
+      setLocation(true)
+    })
+  }, [])
+        if(location == false){
+          return (
+            <>
+            <p className={styles.needloc}>Oi! precisamos da sua localização :D</p>
+            </>
+          )
+        }
 
   return (
     <header className={styles.headerContainer}>
