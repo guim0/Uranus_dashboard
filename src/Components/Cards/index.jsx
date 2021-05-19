22695e72da8207b86ca21c97928960931e28a0dc
Oaparecido <daniel.aparecido4683@gmail.com>
2021-05-19 15:27:37 -0300

    adding get by date

--- src/Components/Cards/index.jsx
+++ src/Components/Cards/index.jsx
@@ -16,70 +16,68 @@ import styles from './styles.module.scss';
 
 
 export function Cards() {
-
+  
   const [hasLocation, setHasLocation] = useState (false);
-  const [temp, setTemp] = useState(0);
-  const [wind, setWind] = useState(0);
-  const [humidity, setHumidity] = useState(0);
-  const [deg, setDeg] = useState(0)
-  const [pluv, setPluv] = useState (12)
- 
+  const [temperature, setTemperature] = useState(0);
+  const [velocity, setVelocity] = useState(0);
+  const [moisture, setMoisture] = useState(0);
+  const [direction, setDirection] = useState(0)
+  const [pluviometter, setPluviometter] = useState (12)
+  const [dateSelected, setDateSelected] = useState(new Date());
 
   useEffect(() =>{
-    navigator.geolocation.getCurrentPosition((position)=>{
-      console.log(position.coords.latitude, position.coords.longitude);
-      axios.get("http://localhost:3303/api/mqtt", {
-        params: {
-          lat: position.coords.latitude,
-          lon: position.coords.longitude,
-          appId: process.env.REACT_APP_OPEN_WHETHER_KEY,
-          lang: 'pt',
-          units: 'Metric'
+    navigator.geolocation.getCurrentPosition(async (position)=>{
+      const date = dateSelected.toISOString().split('T')[0]
+
+      let params = {}
+
+      try {
+        const res = await axios.get(`http://localhost:3303/api/mqtt/${date}`);
+        
+        if (res.data && res.data.params) {
+          const { data } = res;
+          params = data.params;
         }
-        
-      }).then(res => {
-        console.log(res)
-        const { moisture, pluviometter, temperature } = res.data.params
-        const { direction , velocity} = res.data.params.wind
-        
-        setTemp(temperature);
-        setHumidity(moisture);
-        setWind(velocity);
-        setDeg(direction.replace('<', ''));
-        setPluv(pluviometter);
-        
-      })
+      } catch (e) {
+      }
+      
+      setTemperature(params.temperature ? params.temperature : 0);
+      setMoisture(params.moisture ? params.moisture : 0);
+      setVelocity(params.wind ? params.wind.velocity : 0);
+      setDirection(params.wind ? params.wind.direction.replace('<', '') : 0);
+      setPluviometter(params.pluviometter ? params.pluviometter : 0);
       setHasLocation(true)
     })
-
-  }, [])
-        if(hasLocation == false){
-          return (
-            <>
-            <p className={styles.needloc}>Oi! precisamos da sua localização :D</p>
-            </>
-          )
-        }
+  }, [dateSelected])
+  if(hasLocation == false){
+    return (
+        <>
+          <p className={styles.needloc}>Oi! precisamos da sua localização :D</p>
+        </>
+    )
+  }
 
   return (
     <>
        <div className={styles.container}>
 
          <div className={styles.leftSide}>
-         <Climate temp={temp} />
-         <Moisture humidity={humidity}/>
-         <Windspeed wind={wind}/>
-         <WindDir deg={deg}/>
+         <Climate temp={temperature} />
+         <Moisture humidity={moisture}/>
+         <Windspeed wind={velocity}/>
+         <WindDir deg={direction}/>
          </div>
 
          <div className={styles.rightSide}>
-         <Calendar className="calendar"/>
-         <PluvIndex pluv={pluv}/>
+         <Calendar 
+          className="calendar"
+          onChange={(value) => setDateSelected(value)}
+          value={dateSelected}
+          />
+         <PluvIndex pluv={pluviometter}/>
          </div>
        </div>
        </>
-
-
   );
 }
 
